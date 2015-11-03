795a3ce39dcc067f86c4d895df6046876bd5921d
pirates19 <nicholas.ellul@ocsbstudent.ca>
2015-11-03 14:44:14 -0500

    Done

---
+++ 404GradeAverageProgram/MarkToAverageForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace _404GradeAverageProgram
+{
+    partial class MarkToAverageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.btnCaluclate = new System.Windows.Forms.Button();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(53, 101);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(30, 47);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(155, 40);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "Enter a mark to get \r\nits percentage value.";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(214, 24);
+            this.mnuStrip.TabIndex = 3;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // btnCaluclate
+            // 
+            this.btnCaluclate.Location = new System.Drawing.Point(53, 138);
+            this.btnCaluclate.Name = "btnCaluclate";
+            this.btnCaluclate.Size = new System.Drawing.Size(100, 23);
+            this.btnCaluclate.TabIndex = 5;
+            this.btnCaluclate.Text = "Calculate";
+            this.btnCaluclate.UseVisualStyleBackColor = true;
+            this.btnCaluclate.Click += new System.EventHandler(this.btnCaluclate_Click);
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // MarkToAverageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 168);
+            this.Controls.Add(this.btnCaluclate);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "MarkToAverageForm";
+            this.Text = "Convert";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.Button btnCaluclate;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ 404GradeAverageProgram/MarkToAverageForm.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Created by: Nicholas  Ellul
+ * Created on: 03-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 4-04
+ * This program converts a mark to percentage ex; 4+ to 95%
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _404GradeAverageProgram
+{
+    public partial class MarkToAverageForm : Form
+    {
+        public MarkToAverageForm()
+        {
+            InitializeComponent();
+        }
+
+        public int markToInt(string mark)
+        {
+            int markAsPercent;
+
+            switch (mark)
+            {
+                case "4+":
+                    markAsPercent = 95;
+                    break;
+
+                case "4":
+                    markAsPercent = 90;
+                    break;
+
+                case "4-":
+                    markAsPercent = 85;
+                    break;
+
+                case "3+":
+                    markAsPercent = 77;
+                    break;
+
+                case "3":
+                    markAsPercent = 75;
+                    break;
+
+                case "3-":
+                    markAsPercent = 72;
+                    break;
+
+                case "2+":
+                    markAsPercent = 67;
+                    break;
+
+                case "2":
+                    markAsPercent = 65;
+                    break;
+
+                case "2-":
+                    markAsPercent = 62;
+                    break;
+
+                case "1+":
+                    markAsPercent = 57;
+                    break;
+
+                case "1":
+                    markAsPercent = 55;
+                    break;
+
+                case "1-":
+                    markAsPercent = 52;
+                    break;
+
+                case "R":
+                    markAsPercent = 30;
+                    break;
+
+                case "r":
+                    markAsPercent = 30;
+                    break;
+
+                case "NE":
+                    markAsPercent = 0;
+                    break;
+
+                case "ne":
+                    markAsPercent = 0;
+                    break;
+
+                default:
+                    markAsPercent = -1;
+                    break;
+            }
+
+            return markAsPercent;
+        }
+        private void btnCaluclate_Click(object sender, EventArgs e)
+        {
+            string markInput = this.txtInput.Text;
+            int markAsInterger;
+
+            markAsInterger = markToInt(markInput);
+            MessageBox.Show("Your mark is " + markAsInterger + "%", "Conversion");          
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
